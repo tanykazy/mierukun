766d151f6393bb48aeb83deba8672942411e26ff
tanykazy <tanykazy@gmail.com>
2024-08-31 00:42:31 +0900

    Debug

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.post('/predict', async (req, res) => {
         //     endpoint,
         //     instances,
         // };
-        console.log(req);
-        console.log(req.body);
+        console.log(JSON.stringify(req));
+        console.log(JSON.stringify(req.body));
 
         // console.log(textPart);
         // console.log(audioPart);
@@ -47,7 +47,8 @@ app.post('/predict', async (req, res) => {
         res.json(response);
     } catch (err) {
         console.error(err);
-        res.status(500).send('予測中にエラーが発生しました');
+        res.status(500)
+            .send('予測中にエラーが発生しました');
     }
 });
 
@@ -59,14 +60,18 @@ const { VertexAI } = require('@google-cloud/vertexai');
  * TODO(developer): Update these variables before running the sample.
  */
 async function createNonStreamingMultipartContent(
+    contents,
     projectId = 'mierukun-github',
     location = 'us-central1',
     model = 'gemini-1.5-pro',
-    image = 'gs://generativeai-downloads/images/scones.jpg',
-    mimeType = 'image/jpeg'
+    // image = 'gs://generativeai-downloads/images/scones.jpg',
+    // mimeType = 'image/jpeg'
 ) {
     // Initialize Vertex with your Cloud project and location
-    const vertexAI = new VertexAI({ project: projectId, location: location });
+    const vertexAI = new VertexAI({
+        project: projectId,
+        location: location
+    });
 
     // Instantiate the model
     const generativeVisionModel = vertexAI.getGenerativeModel({
@@ -74,19 +79,22 @@ async function createNonStreamingMultipartContent(
     });
 
     // For images, the SDK supports both Google Cloud Storage URI and base64 strings
-    const filePart = {
-        fileData: {
-            fileUri: image,
-            mimeType: mimeType,
-        },
-    };
+    // const filePart = {
+    //     fileData: {
+    //         fileUri: image,
+    //         mimeType: mimeType,
+    //     },
+    // };
 
-    const textPart = {
-        text: 'what is shown in this image?',
-    };
+    // const textPart = {
+    //     text: 'what is shown in this image?',
+    // };
 
+    // const request = {
+    //     contents: [{ role: 'user', parts: [filePart, textPart] }],
+    // };
     const request = {
-        contents: [{ role: 'user', parts: [filePart, textPart] }],
+        contents: contents,
     };
 
     console.log('Prompt Text:');
