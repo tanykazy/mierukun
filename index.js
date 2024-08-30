ebe64bfb4f265fff0b9d7d2f9a4f43e9cd86e8a1
tanykazy <tanykazy@gmail.com>
2024-08-31 02:04:01 +0900

    Debug

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 
 app.use(express.static("dist/mierukun"));
+app.use(express.json());
+
 app.get('/', function (req, res) {
     res.redirect('/');
 });
