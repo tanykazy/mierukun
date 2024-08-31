9c62df1de01f730795d45d85a89aa6ea0fe267d4
tanykazy <tanykazy@gmail.com>
2024-08-31 12:56:12 +0900

    Debug

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var express = require("express");
 var app = express();
 
 app.use(express.static("dist/mierukun"));
-app.use(express.json());
+app.use(express.json({
+    limit: '1gb'
+}));
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', function (req, res) {
