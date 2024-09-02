69e0cfebed3b4d45e58936dba530fc055a34b754
tanykazy <tanykazy@gmail.com>
2024-09-03 02:07:48 +0900

    Debug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -103,7 +103,7 @@ export class AppComponent implements OnInit {
     // console.log('generateContent');
     const response = await this.geminiService.generateContent({
       text: prompt
-    }, await this.geminiService.blobToGenerativePart(blob, 'audio/webm'));
+    }, await this.geminiService.blobToGenerativePart(blob, blob.type));
 
     const text = response;
 
